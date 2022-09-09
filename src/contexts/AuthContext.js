46c589adebe7714da437de91b6b66ac784d4a953
TA-Sakin <tasakin98@gmail.com>
2022-09-10 03:00:05 +0600

    context added for firebase signup

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,52 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  updateProfile,
+} from "firebase/auth";
+import React, { useContext, useEffect, useState } from "react";
+import Loading from "../components/shared/Loading";
+import auth from "../Utils/firebase.init";
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  // signup function
+  async function signup(email, password, username) {
+    await createUserWithEmailAndPassword(auth, email, password);
+
+    // update profile
+    await updateProfile(auth.currentUser, {
+      displayName: username,
+    });
+
+    const user = auth.currentUser;
+    setCurrentUser({
+      ...user,
+    });
+  }
+
+  const value = {
+    loading,
+    currentUser,
+    signup,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {loading ? <Loading /> : children}
+    </AuthContext.Provider>
+  );
+}
